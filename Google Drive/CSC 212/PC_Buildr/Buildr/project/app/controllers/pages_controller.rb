b8a09b8692529883db8370ed01479485655fc49a
Brandon Slaght <brandonsesipod@gmail.com>
2015-11-21 06:55:44 +0000

    figured out how to add new page

--- Google Drive/CSC 212/PC_Buildr/Buildr/project/app/controllers/pages_controller.rb
+++ Google Drive/CSC 212/PC_Buildr/Buildr/project/app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
     def home
         puts "PC Buildr, bitches"
     end
+    
+    def specs
+        puts "Specs, bitches"
+    end
 end
--- Google Drive/CSC 212/PC_Buildr/Buildr/project/config/routes.rb
+++ Google Drive/CSC 212/PC_Buildr/Buildr/project/config/routes.rb
@@ -54,4 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root to: 'pages#home'
+  get '/specs' => 'pages#specs'
 end
