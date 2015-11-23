230e75e012ec61f403a7b77f364038ed7ff141a1
ferozrauf <feroz.rauf@smartfinancellc.net>
2015-11-23 15:12:36 -0500

    Adding DB sequel initalize to different file

--- Google Drive/CSC 212/PC_Buildr/Buildr/project/config/application.rb
+++ Google Drive/CSC 212/PC_Buildr/Buildr/project/config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require "rubygems"
+require "sequel"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +23,7 @@ module Project
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    DB = Sequel.sqlite 
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
