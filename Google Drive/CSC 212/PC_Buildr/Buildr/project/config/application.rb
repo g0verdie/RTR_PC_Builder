6247053202841edfb98b847e04c8a1becbaac1ec
dev_idyskin <idyskin@u.rochester.edu>
2015-11-30 15:51:43 -0500

    THIS WORKS

--- Google Drive/CSC 212/PC_Buildr/Buildr/project/config/application.rb
+++ Google Drive/CSC 212/PC_Buildr/Buildr/project/config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require "rubygems"
-require "sequel"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,7 +22,5 @@ module Project
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    DB = Sequel.sqlite 
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
