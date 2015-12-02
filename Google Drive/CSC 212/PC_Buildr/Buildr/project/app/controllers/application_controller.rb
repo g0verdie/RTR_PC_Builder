1a254277655d1cce36aafe2dc7efba9805d422e1
dev_idyskin <idyskin@u.rochester.edu>
2015-12-01 22:23:32 -0500

    added build logic to app controller

--- Google Drive/CSC 212/PC_Buildr/Buildr/project/app/controllers/application_controller.rb
+++ Google Drive/CSC 212/PC_Buildr/Buildr/project/app/controllers/application_controller.rb
@@ -5,7 +5,27 @@ class ApplicationController < ActionController::Base
   helper_method :test
 
   def test
-            $price = params[:price]
-            return $price
+            $price = params[:price] * .8
+            if params[:type] == "casual"
+            	$misc_price = $price * .5
+            	$price = $price * .5
+            	$processor = ($price * params[:processor].to_f)/100
+            	$storage = ($price * params[:storage].to_f)/100
+            elsif params[:type] == "gaming"
+            	$misc_price = $price * .3
+            	$price = $price * .7
+            	$processor = ($price * params[:processor].to_f)/100
+            	$storage = ($price * params[:storage].to_f)/100
+            	$graphics = ($price * params[:graphics].to_f)/100
+            else
+            	$misc_price = $price * .35
+            	$price = $price * .65
+            	$processor = ($price * params[:processor].to_f)/100
+            	$storage = ($price * params[:storage].to_f)/100
+            	$graphics = ($price * params[:graphics].to_f)/100
+            end
+
+            $size = params[:case]
+    		return "hello world"
         end
 end
