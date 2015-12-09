b0bc0c4249fafcfff6ff81ebcebdb9c8dc17438c
dev_idyskin <idyskin@u.rochester.edu>
2015-12-09 16:11:10 -0500

    results changes

--- Google Drive/CSC 212/PC_Buildr/Buildr/project/app/controllers/pages_controller.rb
+++ Google Drive/CSC 212/PC_Buildr/Buildr/project/app/controllers/pages_controller.rb
@@ -19,10 +19,16 @@ class PagesController < ApplicationController
     
     def results
         puts "Results, bitches"
+
         if params[:type] == nil
             redirect_to :home
         elsif params[:case] == nil
             redirect_to :back
         end
+        def save_my_previous_url
+            # session[:previous_url] is a Rails built-in variable to save last url.
+            session[:my_previous_url] = URI(request.referer || '').path
+        end
+        self.save_my_previous_url
     end
 end
